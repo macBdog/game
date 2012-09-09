9ed87db5692540ebac0e0e1607e9d9930e489004
Charles <charles@henden.com.au>
2012-09-09 10:39:02 -0700

    Stub out model management.

---
+++ engine/ModelManager.cpp
@@ -0,0 +1,125 @@
+#include "FileManager.h"
+#include "Log.h"
+
+#include "ModelManager.h"
+
+template<> ModelManager * Singleton<ModelManager>::s_instance = NULL;
+
+const unsigned int ModelManager::s_modelPoolSize = 65536;		// 64 meg for models
+
+const float ModelManager::s_updateFreq = 1.0f;
+
+ModelManager::ModelManager(float a_updateFreq)
+	: m_updateFreq(a_updateFreq)
+	, m_updateTimer(0.0f)
+{
+}
+
+bool ModelManager::Startup(const char * a_modelPath)
+{
+	// Reset update timer in case we have been shutdown the re started
+	 m_updateTimer = 0;
+
+	// Init model memory pool
+	m_modelPool.Init(s_modelPoolSize);
+
+	// This can be removed in all but DEBUG configuration, but its nice when viewing memory
+	memset(m_modelPool.GetHead(), 0, m_modelPool.GetAllocationSizeBytes());
+
+	// Cache off the model path for non qualified addressing of models
+	memset(&m_modelPath, 0 , StringUtils::s_maxCharsPerLine);
+	strncpy(m_modelPath, a_modelPath, strlen(a_modelPath));
+
+	return true;
+}
+
+bool ModelManager::Shutdown()
+{
+	// Cleanup memory
+	m_modelPool.Done();
+
+	return true;
+}
+
+bool ModelManager::Update(float a_dt)
+{
+	if (m_updateTimer < m_updateFreq)
+	{
+		m_updateTimer += a_dt;
+		return false;
+	}
+	else // Due for an update, scan all models
+	{
+		m_updateTimer = 0.0f;
+		bool modelReloaded = false;
+
+		// Each model in the pool gets tested
+		ManagedModel * curModel = NULL;
+		while ( m_modelMap.GetNext(curModel) && curModel != NULL)
+		{
+			unsigned int curTimeStamp = FileManager::Get().GetFileTimeStamp(curModel->m_path);
+			if (curTimeStamp > curModel->m_timeStamp)
+			{
+				Log::Get().Write(Log::LL_INFO, Log::LC_ENGINE, "Change detected in model %s, reloading.", curModel->m_path);
+				modelReloaded = curModel->m_model.Load(curModel->m_path);
+				curModel->m_timeStamp = curTimeStamp;
+			}
+		}
+		
+		return modelReloaded;
+	}
+}
+
+Model * ModelManager::GetModel(const char * a_modelPath)
+{
+	// Model paths are either fully qualified or relative to the config model dir
+	char fileNameBuf[StringUtils::s_maxCharsPerLine];
+	if (!strstr(a_modelPath, ":\\"))
+	{
+		sprintf(fileNameBuf, "%s%s", m_modelPath, a_modelPath);
+	} 
+	else // Already fully qualified
+	{
+		sprintf(fileNameBuf, "%s", a_modelPath);
+	}
+	
+	// Get the identifier for the new model
+	StringHash modelHash(fileNameBuf);
+	unsigned int modelId = modelHash.GetHash();
+
+	// If it already exists
+	if (IsModelLoaded(modelId))
+	{
+		// Just returned the cached copy
+		ManagedModel * foundModel = NULL;
+		m_modelMap.Get(modelId, foundModel);
+		return &foundModel->m_model;
+	}
+	else if (ManagedModel * newModel = m_modelPool.Allocate(sizeof(ManagedModel)))
+	{
+		// Insert the newly allocated model
+		newModel->m_model.Load(fileNameBuf);
+		newModel->m_timeStamp = FileManager::Get().GetFileTimeStamp(fileNameBuf);
+		sprintf(newModel->m_path, "%s", fileNameBuf);
+		m_modelMap.Insert(modelId, newModel);
+		return &newModel->m_model;
+	}
+	else // Report the error
+	{
+		Log::Get().Write(Log::LL_ERROR, Log::LC_ENGINE, "Model allocation failed for %s, fileNameBuf");
+		return NULL;
+	}
+   return NULL;
+}
+
+bool ModelManager::IsModelLoaded(unsigned int a_modelPathHash)
+{
+	// Look through map for the target model
+	ManagedModel * foundModel = NULL;
+	if (m_modelMap.Get(a_modelPathHash, foundModel))
+	{
+		return true;
+	}
+	
+	return false;
+}
---
+++ engine/ModelManager.h
@@ -0,0 +1,78 @@
+#ifndef _ENGINE_MODEL_MANAGER_H_
+#define _ENGINE_MODEL_MANAGER_H_
+
+#include "../core/HashMap.h"
+#include "../core/LinearAllocator.h"
+
+#include "Singleton.h"
+#include "StringHash.h"
+#include "StringUtils.h"
+#include "Model.h"
+
+//\brief ModelManager keeps track of all models in the game and the memory
+//		 required for them. It handles hot loading of all model resources
+//		 and will only load a unique model by path once.
+class ModelManager : public Singleton<ModelManager>
+{
+public:
+	
+	//\brief Ctor calls through to startup
+	ModelManager(float a_updateFreq = s_updateFreq);
+	~ModelManager() { Shutdown(); }
+
+	//brief Initialise memory pools on startup, cleanup models on shutdown
+	bool Startup(const char * a_modelPath);
+	bool Shutdown();
+
+	//\brief Update will poll for model changes and reload any models that have a newer version than on disk
+	//\return true if a model was old and needed to be reloaded
+	bool Update(float a_dt);
+
+	//\brief Get or load a TGA file into model memory
+	//\param a_tgaPath cstring to identify the model by
+	//\return model ID of the identified model
+	Model * GetModel(const char *a_modelPath);
+
+	//\brief Functions to check if a model has already been loaded
+	//\param a_tgaPathHash is the identified for the model
+	//\return -1 the category that the model is loaded into, none if not loaded
+	bool IsModelLoaded(unsigned int a_tgaPathHash);
+	inline bool IsModelLoaded(const char *a_tgaPath) { return IsModelLoaded(StringHash(a_tgaPath).GetHash()); }
+
+	//\brief Reload a single model without changing the IDs
+	//\param a_cat the category the model is found in. If not supplied, an exhaustive search is performed
+	//\return true in the model was found and reloaded successfully
+	bool ReloadModel(unsigned int a_modelPathHash);
+	inline bool Reloadmodel(const char *a_modelPath) { return ReloadModel(StringHash(a_modelPath).GetHash()); }
+
+	//\brief Wholesale reload of models
+	bool ReloadAllModels();
+
+	//\brief Get the fully qualified model path
+	//\return A pointer to a c string containing the model path
+	inline const char * GetModelPath() { return m_modelPath; }
+	
+private:
+
+	static const unsigned int s_modelPoolSize;					///< How much memory is assigned for each category
+	static const float s_updateFreq;							///< How often the model manager should check for updates
+
+	//\brief A managed model contains the actual model data as well as extra information
+	//		 that enables it to be version checked and hot reloaded 
+	struct ManagedModel
+	{
+		Model  m_model;											///< The actual model
+		unsigned int m_timeStamp;								///< Datestamp for checking a newer version
+		char m_path[StringUtils::s_maxCharsPerLine];			///< The full path for reloading
+	};
+
+	typedef HashMap<unsigned int, ManagedModel *> modelMap;
+
+	LinearAllocator<ManagedModel> m_modelPool;					///< Memory pool for each model category
+	modelMap m_modelMap;										///< List of models for each category
+	char m_modelPath[StringUtils::s_maxCharsPerLine];			///< Cache off model path 
+	float m_updateFreq;											///< How often the model manager should check for changes
+	float m_updateTimer;										///< If we are due for a scan and update of models
+};
+
+#endif /* _ENGINE_MODEL_MANAGER_H_ */
