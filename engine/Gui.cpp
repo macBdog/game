beeb23236c0fe9f2aea524dfefcc372a0ba5a622
Charles <charles@henden.com.au>
2013-07-15 23:25:15 -0700

    Execute menu LUA code onload

--- engine/Gui.cpp
+++ engine/Gui.cpp
@@ -325,24 +325,30 @@ bool Gui::LoadMenu(const char * a_menuFile)
 				// Load the script
 				if (GameFile::Property * scriptProp = menuFile->FindProperty(menuObject, "script"))
 				{
-					lua_State *lua = luaL_newstate();  // Open Lua
-					luaopen_io (lua);              // Load io library
+					// Open Lua and load the standard libraries
+					lua_State *lua = luaL_newstate();	
+					luaL_requiref(lua, "io", luaopen_io, 1);
+					luaL_requiref(lua, "base", luaopen_base, 1);
+					luaL_requiref(lua, "table", luaopen_table, 1);
+					luaL_requiref(lua, "string", luaopen_string, 1);
+					luaL_requiref(lua, "math", luaopen_math, 1);
 
 					// Construct path to script using menu path
 					char scriptPath[StringUtils::s_maxCharsPerLine];
 					sprintf(scriptPath, "%sscripts\\%s", m_guiPath, menuFile->GetString("menu", "script"));
 					if (luaL_loadfile(lua, scriptPath) == 0)
 					{
-					   // Call main...
-					   if (lua_pcall(lua, 0, LUA_MULTRET, 0) == 0)
-					   { 
-						  // Push the function name onto the stack
-						  lua_pushstring(lua, "UpdatePower");
-
-						  // Function is located in the Global Table
-						  lua_gettable(lua, -2);  
-						  lua_pcall(lua, 0, 0, 0);
+					   // Call code located in script file...
+					   if (lua_pcall(lua, 0, LUA_MULTRET, 0) != 0)
+					   {
+						   // Report ant remove error message from stack
+						   Log::Get().Write(Log::LL_ERROR, Log::LC_GAME, lua_tostring(lua, -1));
+						   lua_pop(lua, 1);
 					   }
+					}
+					else
+					{
+						Log::Get().Write(Log::LL_ERROR, Log::LC_GAME, "Cannot find script file %s referred to by gui file %s.", scriptPath, a_menuFile);
 					}
 					lua_close (lua);
 				}
